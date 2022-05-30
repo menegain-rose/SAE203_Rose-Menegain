5126a542714117b8438cd4a55bc0776f5b690215
menegain-rose <rose.menegain@edu.univ-fcomte.fr>
2022-05-30 08:57:50 +0200

    logo

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,39 @@
     <nav>
       <RouterLink to="/" class="text-blue-600 underline">Accueil</RouterLink>
     </nav>
+
+      <header class="bg-black-primary flex justify-between">
+    <img class="w-24 h-auto" src="/images-icones/Logoblanc.svg" alt="">
+    <section>
+      <div  class="w-max ">
+        <Profil class="h-12 w-auto mr-1 inline-block fill-blanc-primary"/> 
+      </div>
+      
+      <!-- menu -->
+      <nav>
+          <div  class="w-max ">
+            <Menu class="h-12 w-auto mr-1 inline-block fill-blanc-primary"/>
+              <div class="text-blanc-primary">
+                <router-link to="/">Accueil</router-link>
+                <router-link to="/Apropos">A propos</router-link>
+                <router-link to="/compteArtiste">Votre Compte</router-link>
+                <router-link to="/Dons">Dons</router-link>
+                <router-link to="/FAQ">FAQ</router-link>
+                <router-link to="/Inscription">Inscription</router-link>
+                <router-link to="/Messagerie">Messagerie</router-link>
+                <router-link to="/MLegal">Mentions légales</router-link>
+                <router-link to="/NosArtistes">Nos Artistes</router-link>
+              </div>
+        </div>
+      </nav>
+    </section>
+  </header>
   </header>
 
   <RouterView />
 
   <footer>
-    
+
   </footer>
 </template>
 
