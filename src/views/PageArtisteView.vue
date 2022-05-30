d85a2b70f8053dc66ad78c4509edefc759581ca2
menegain-rose <rose.menegain@edu.univ-fcomte.fr>
2022-05-30 14:45:07 +0200

    police principale qui marche

--- src/views/PageArtisteView.vue
+++ src/views/PageArtisteView.vue
@@ -1,6 +1,18 @@
 <template>
   <main>
-    <h1 class="text-2xl">Bonjour</h1>
+    <h2 class="text-2xl">Le Tigre</h2>
+    <img src="/images-tigre/Tigre.png" alt="">
+    <h3>Vendredi 15 juillet</h3>
+    <p>
+      Punk rock, dance-punk, rock indépendant, electroclash. <br>
+      Un groupe originaire de New-York et composé de Kathleen Hanna, Johanna Fateman et JD Samson.
+    </p>
+
+     <iframe width="560" height="315" src="https://www.youtube.com/embed/wjNln9mXuTI" 
+     title="YouTube video player" frameborder="0" 
+     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+     </iframe>
+
   </main>
 </template>
 
