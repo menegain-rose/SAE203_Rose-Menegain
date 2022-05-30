9675f8e1299a8f1f47d1320a755d0bfddf19b98c
menegain-rose <rose.menegain@edu.univ-fcomte.fr>
2022-05-30 16:09:21 +0200

    page mentions légales

--- src/views/MentionsLegalsView.vue
+++ src/views/MentionsLegalsView.vue
@@ -2,6 +2,167 @@
   <main class="p-8">
     <h2 class="text-2xl">Mentions Légales</h2>
 
+    <h4 class="my-3">
+      CONTACT
+    </h4>
+
+    <p>
+      Pour toute question liée au festival, merci de contacter l’adresse : info@bonyfest.fr <br>
+      Pour envoyer dossier de presse et cd de présentation de votre groupe, une adresse : <br>
+      Bonyfest Productions, Strasbourg 67 000  – France <br>
+      Tél +33 (0)3 39 48 14 08
+    </p>
+
+    <h4 class="my-3">
+      ÉDITEUR
+    </h4>
+
+    <p>
+      BONYFEST PRODUCTIONS (Association Loi 1901) <br>
+      Siège social : Bonyfest Productions, Strasbourg 67 000  – France <br>
+      Tel : +33 (0)3 39 48 14 08 <br>
+      Licences entrepreneur de spectacles : PLATESV-R-2021-011302 / PLATESV-R-2021-011306 / PLATESV-R-2021-011308
+    </p>
+
+    <h4 class="my-3">
+      HEBERGEMENT
+    </h4>
+
+    <p>
+      niwanet.net
+    </p>
+
+    <h4 class="my-3">
+      DEVELOPPEMENT DU SITE
+    </h4>
+
+    <p>
+      Design : <br>
+      DA / UI : Cassandra Benric <br>
+      UX / PM : Christophe Penrer <br>
+      Développement <br>
+      Front: Francis Girand <br>
+      Back: Emma Bornet <br>
+    </p>
+
+    <h4 class="my-3">
+      FORMALITES DE DECLARATION
+    </h4>
+
+    <p>
+      Le présent site Internet et notamment le traitement automatisé de l’information 
+      ont fait l’objet d’une déclaration formelle auprès de la Commission Nationale de l’Informatique et des Libertés (CNIL). <br>
+      Pour plus d’informations, merci de prendre contact avec Bonyfest Productions : <br>
+      T. +33 (0)3 39 48 14 08 / info@bonyfest.fr
+    </p>
+
+    <h4 class="my-3">
+      DROITS D’AUTEUR
+    </h4>
+
+    <p>
+      Ce site est propriété de l’association Bonyfest Productions – la réservation des noms de domaine www.bonyfest.fr, 
+      www.bonyshop.com, ont été effectués conformément aux dispositions de l’AFNIC.
+    </p>
+
+    <p>
+      L’ensemble de ce site relève de la législation française et internationale sur le droit d’auteur et la propriété industrielle. <br>
+      Tous les droits de reproduction sont réservés, y compris les représentations iconographiques et photographiques. La reproduction de tout ou partie de ce site sur un support quel qu’il soit, 
+      est formellement interdite sauf autorisation de Bonyfest productions.
+    </p>
+
+    <p>
+      La reproduction des textes sur un support papier est autorisée sous réserve du respect des trois conditions suivantes : <br>
+    </p>
+
+    <p>
+      -  gratuité de la diffusion ;
+    </p>
+
+    <p>
+      - respect de l’intégrité des documents reproduits (pas de modification ni altération) ;
+    </p>
+
+    <p>
+      la citation claire et lisible de la source sous la forme suivante: « Document issu du site Internet www.hellfest.fr Bonyfest Open Air » Les droits de reproduction sont réservés et strictement limités.
+    </p>
+
+    <h4 class="my-3">
+      POLITIQUE DE CONFIDENTIALITE DES DONNEES
+    </h4>
+
+    <p>
+      L’association BONYFEST PRODUCTIONS traite les données personnelles avec grand respect. 
+      C’est-à-dire que toutes les informations personnelles que vous avez partagées ou partagerez avec BONYFEST PRODUCTIONS seront conservées en toute sécurité. Il n’y a aucune vente de données à une autre entreprise, qu’elle soit partenaire ou non. Ci-dessous, 
+      en savoir plus sur l’utilisation, mais aussi la conservation de vos données personnelles.
+    </p>
+
+    <h4 class="my-3">
+      INFORMATIONS NOMINATIVES
+    </h4>
+
+    <p>
+      Les informations que nous recueillons sont destinées à mieux vous connaître, 
+      ne seront en aucun cas transmises à des tiers (exception faite des autorités dûment habilités en vertu de dispositions légales et règlementaires). <br>
+      Conformément à la loi « Informatique et Libertés » du 6 janvier 1978, qui intègre le RGPD (Règlement Général européen sur la Protection des Données) depuis le 25 mai 2018, vous disposez d’un droit d’accès, de modification, 
+      de rectification et de suppression des données qui vous concernent.
+    </p>
+
+    <p>
+      Pour l’exercer, il vous suffit d’envoyer un e-mail à info@bonyfest.fr pour objet : DONNEES PERSONNELLES
+    </p>
+
+    <p>
+      Informations générales sur les achats en ligne :
+      Lorsque vous passez commande sur notre site web de billetterie, vous renseignez des informations personnelles 
+      (Noms, prénoms, adresse email, civilité, pays d’origine, numéros de téléphone, code postal, ville, date de naissance). <br>
+      Ces informations sont conservées par Weezevent, en toute sécurité. Pour en savoir plus, veuillez consulter la politique de confidentialité de Weezevent. <br>
+      Seuls Weezevent et le service billetterie de HELLFEST PRODUCTIONS peuvent accéder à ces données, 
+      qui ne seront jamais cédées à une tierce entreprise (sauf exigence légale ou règlementaire).
+    </p>
+
+    <h4 class="my-3">
+      SECURITE
+    </h4>
+
+    <p>
+      La billetterie en ligne utilise la technologie https, qui encrypte toutes les informations qui transitent entre vos appareils et le site internet. <br>
+      Les certificats SSL vous assurent que les pages que vous visitez proviennent bien de notre site et permettent ainsi d’éviter la fraude.
+    </p>
+
+    <p>
+      Confidentialité des courriers électroniques
+      Concernant tout courrier électronique adressé à BONYFEST PRODUCTIONS à travers ce site, merci de prendre note du fait que :
+    </p>
+
+    <p>
+        - Le secret des correspondances transmises par le réseau Internet n’est pas garanti ;
+    </p>
+
+    <p>
+      - Les courriers électroniques sont conservés sur support informatique, au moins le temps nécessaire à l’administration pour fournir une réponse ou pour traiter votre demande.
+    </p>
+
+    <h4 class="my-3">
+      COOKIES / STATISTIQUES
+    </h4>
+
+    <p>
+      Les « cookies » sont des éléments d’information qu’un site Internet transmet au disque dur d’un ordinateur individuel afin de garder un enregistrement du passage ; <br>
+      les mots de passe ne sont pas gardés dans les « cookies ». Les « cookies » permettent aux utilisateurs de conserver la mémoire de leurs paramètres personnels pour accéder à ce site et ainsi faciliter leur navigation, mais aussi d’obtenir des statistiques. <br>
+      Si vous ne souhaitez pas recevoir de « cookie » vous pouvez utiliser l’option sur votre navigateur qui rend indisponible les « cookies ».
+    </p>
+
+    <p>
+      Contenu embarqué depuis d’autres sites <br>
+      Les articles de ce site peuvent inclure des contenus intégrés (par exemple des vidéos, images, articles…). <br>
+      Le contenu intégré depuis d’autres sites se comporte de la même manière que si le visiteur se rendait sur cet autre site.
+    </p>
+
+    <p>
+      Ces sites web pourraient collecter des données sur vous, utiliser des cookies, embarquer des outils de suivis tiers, suivre vos interactions avec ces contenus embarqués si vous disposez d’un compte connecté sur leur site web.
+    </p>
+
 
 
 
